74243e75b5256852954996f65675d963809313d7
Heba Jammoul <heba.jammoul.1984@gmail.com>
2023-06-29 17:04:43 +0200

    Mettre à jour le code HTML

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,13 @@
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+const taglines = [ //Toutes les taglines pour le carrousel
+
+	'Impressions tous formats <span>en boutique et en ligne</span>'
+	,
+
+	'Tirages haute définition grand format <span>pour vos bureaux et events</span>'
+	,
+
+	'Grand choix de couleurs <span>de CMJN aux pantones</span>'
+	,
+
+	'Autocollants <span>avec découpe laser sur mesure</span>'
 ]
