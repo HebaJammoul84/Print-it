95680244d853210742e6f7e80cec44163ff8c712
Heba Jammoul <heba.jammoul.1984@gmail.com>
2023-06-29 22:24:50 +0200

    Ajouter des conditions

--- assets/script.js
+++ assets/script.js
@@ -30,3 +30,27 @@ precedent.addEventListener('click', function () {
 function plusSlides(n) {
 	slidesActive(slideIndex += n);
 }
+
+function slidesActive(n) {
+	let slides = document.querySelectorAll('.banner-img');
+	let dots = document.querySelectorAll('.dot');
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].classList.remove('dot_selected');
+	}
+
+	slides[slideIndex - 1].style.display = "block";
+
+	dots[slideIndex - 1].classList.add('dot_selected');
+
+	let tagline = taglines[slideIndex - 1];
+
+	let taglineElement = document.querySelector('.tagline');
+	taglineElement.innerHTML = tagline;
+}
